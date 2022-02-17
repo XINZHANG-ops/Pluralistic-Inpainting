d2af468c13944c24175127fbdfaf03cb7e5d724d
XINZHANG-ops <47906299+XINZHANG-ops@users.noreply.github.com>
2022-02-16 20:48:25 -0500

    Add files via upload

--- options/test_options.py
+++ options/test_options.py
@@ -7,10 +7,10 @@ class TestOptions(BaseOptions):
 
         parser.add_argument('--ntest', type=int, default=float("inf"), help='# of the test examples')
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here')
-        parser.add_argument('--how_many', type=int, default=50, help='how many test images to run')
+        parser.add_argument('--how_many', type=int, default=10, help='how many test images to run') # 50
         parser.add_argument('--phase', type=str, default='test', help='train, val, test')
-        parser.add_argument('--nsampling', type=int, default=50, help='ramplimg # times for each images')
-        parser.add_argument('--save_number', type=int, default=10, help='choice # reasonable results based on the discriminator score')
+        parser.add_argument('--nsampling', type=int, default=1, help='ramplimg # times for each images')# 50
+        parser.add_argument('--save_number', type=int, default=1, help='choice # reasonable results based on the discriminator score')# 10
 
         self.isTrain = False
 
